55c6bd465410e22d31d4c43df7962cfa53f4a8a1
Georgios Schinas <georschi@users.noreply.github.com>
2024-11-12 16:49:19 +0000

    update pipeline to use varialble pipeline name and everything in functions

--- gemini/sample-apps/e2e-gen-ai-app-starter-pack/app/patterns/agent_builder_search/resources_to_copy/data_ingestion/pipeline.py
+++ gemini/sample-apps/e2e-gen-ai-app-starter-pack/app/patterns/agent_builder_search/resources_to_copy/data_ingestion/pipeline.py
@@ -1,4 +1,3 @@
-# install kfp  google-cloud-pipeline-components
 import os
 from google.cloud import aiplatform
 from kfp import compiler, dsl
@@ -12,6 +11,7 @@ data_store_id = os.getenv("DATA_STORE_ID", "No agent builder data store id set")
 service_account = os.getenv("SERVICE_ACCOUNT", "No service account set")
 pipeline_root = os.getenv("PIPELINE_ARTIFACT_OUTPUT", "No pipeline artifact output set")
 cron_schedule = os.getenv("CRON_SCHEDULE", "No cron schedule set")
+pipeline_name = os.getenv("PIPELINE_NAME", "No pipeline name set")
 
 pipeline_root = f"gs://{pipeline_root}/pipeline_root/"
 
@@ -26,7 +26,7 @@ print("cron_schedule: ", cron_schedule)
 
 
 @dsl.pipeline(
-    name="datastore-ingestion-pipeline",
+    name=pipeline_name,
     description="A pipeline to run ingestion of new data into the datastore",
     pipeline_root=pipeline_root,
 )
@@ -45,49 +45,49 @@ def pipeline(
         data_store_id=data_store_id,
     )
 
+if __name__ == "__main__":
 
-compiler.Compiler().compile(
-    pipeline_func=pipeline, package_path="ingestion_pipeline.json"
-)
+    compiler.Compiler().compile(
+        pipeline_func=pipeline, package_path="ingestion_pipeline.json"
+    )
 
+    job = aiplatform.PipelineJob(
+        display_name=pipeline_name,
+        template_path="ingestion_pipeline.json",
+        pipeline_root=pipeline_root,
+        project=project_id,
+        location=region_vertex,
+        parameter_values={
+            "project_id": project_id,
+            "region_vertex": region_vertex,
+            "region_search": region_search,
+            "input_bucket": input_bucket,
+            "data_store_id": data_store_id,
+        },
+    )
 
-DISPLAY_NAME = "ingestion_pipeline_job"
+    pipeline_job_schedule = aiplatform.PipelineJobSchedule(
+        pipeline_job=job,
+        display_name=f"{pipeline_name} Weekly Ingestion Job",
+        credentials=job.credentials,
+        project=job.project,
+        location=job.location,
+    )
 
-job = aiplatform.PipelineJob(
-    display_name=DISPLAY_NAME,
-    template_path="ingestion_pipeline.json",
-    pipeline_root=pipeline_root,
-    project=project_id,
-    location=region_vertex,
-    parameter_values={
-        "project_id": project_id,
-        "region_vertex": region_vertex,
-        "region_search": region_search,
-        "input_bucket": input_bucket,
-        "data_store_id": data_store_id,
-    },
-)
+    schedule_list = pipeline_job_schedule.list(
+        filter=f'display_name="{pipeline_name} Weekly Ingestion Job"',
+        project=project_id,
+        location=region_vertex,
+    )
+    print("Schedule lists found: ", schedule_list)
+    if not schedule_list:
+        pipeline_job_schedule.create(cron=cron_schedule, service_account=service_account)
+        print("Schedule created")
+    else:
+        schedule_list[0].update(cron=cron_schedule)
+        print("Schedule updated")
 
-pipeline_job_schedule = aiplatform.PipelineJobSchedule(
-    pipeline_job=job,
-    display_name="Weekly Ingestion Job",
-    credentials=job.credentials,
-    project=job.project,
-    location=job.location,
-)
-
-schedule_list = pipeline_job_schedule.list(
-    filter='display_name="Weekly Ingestion Job"',
-    project=project_id,
-    location=region_vertex,
-)
-print("Schedule lists found: ", schedule_list)
-if not schedule_list:
-    pipeline_job_schedule.create(cron=cron_schedule, service_account=service_account)
-    print("Schedule created")
-else:
-    schedule_list[0].update(cron=cron_schedule)
-    print("Schedule updated")
-    for schedule in schedule_list[1:]:
-        print("Duplicate schedule found, deleting it")
-        schedule.delete()
+        # In case that multiple schedules are created during testing, you may delete them using the following code
+        # for schedule in schedule_list[1:]:
+        #     print("Duplicate schedule found, deleting it")
+        #     schedule.delete()
